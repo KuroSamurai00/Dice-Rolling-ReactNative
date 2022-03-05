f67e1215c62af7b2ce72d460d6c8435e869a70ac
KuroSamurai00 <Kurosamurai00@gmail.com>
2022-03-05 14:54:01 -0800

    init commit. adding Dice Select file, Dice Screen, Main nav, and json

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView,  } from 'react-native';
 
-export default function App() {
+
+
+const App = props => {
+
+const [diceSelect, setDiceSelect] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <View>
+        <TouchableOpacity onPress={ () => props.navigation.navigate('DiceScreen', {dice4: 4})   } > 
+          <Text style={styles.font}> Dice 4 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+      <View>
+        <TouchableOpacity> 
+          <Text style={styles.font}> Dice 6 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+      <View>
+        <TouchableOpacity> 
+          <Text style={styles.font}> Dice 8 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+
+      <View>
+        <TouchableOpacity> 
+          <Text style={styles.font}> Dice 10 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+      <View>
+        <TouchableOpacity> 
+          <Text style={styles.font}> Dice 20 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+      <View>
+        <TouchableOpacity> 
+          <Text style={styles.font}> Dice 100 </Text> 
+        </TouchableOpacity> 
+      </View>
+
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00c0ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  font:{
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+
 });
+export default App;
---
+++ Nav/Screens/DiceScreen.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Touchable, SafeAreaView} from 'react-native'
+
+const DiceScreen = props => {
+
+    return(
+
+        <SafeAreaView>
+            <View>
+                <Text> Page view </Text>
+            </View>
+
+
+
+        </SafeAreaView>
+
+
+
+
+    );
+
+}
+
